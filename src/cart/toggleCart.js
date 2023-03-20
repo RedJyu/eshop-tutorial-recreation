25f631512a0341b12035d3bab5b332695a2b54aa
RedJyu <piotr.stezycki@protonmai.com>
2023-03-20 11:41:46 +0100

    added cart toggle

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,14 @@
 import { getElement } from '../utils.js';
 
+const cart = getElement('.cart-overlay');
+const close = getElement('.cart-close');
+const toggle = getElement('.toggle-cart');
+
+toggle.addEventListener('click', () => {
+  cart.classList.add('show');
+});
+close.addEventListener('click', () => {
+  cart.classList.remove('show');
+});
+
 export const openCart = () => {};
