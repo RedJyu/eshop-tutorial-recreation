586304e95314ac7b85a8e378a549ef2bd544d803
RedJyu <piotr.stezycki@protonmai.com>
2023-03-20 11:23:09 +0100

    added sidebar toggle

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,12 @@
 import { getElement } from './utils.js';
+
+const toggleNav = getElement('.toggle-nav');
+const sidebar = getElement('.sidebar-overlay');
+const closeBtn = getElement('.sidebar-close');
+
+toggleNav.addEventListener('click', () => {
+  sidebar.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show');
+});
