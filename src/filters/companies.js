0890056a93a5afc202c81382cf037f659217fc0e
RedJyu <piotr.stezycki@protonmai.com>
2023-03-22 13:12:23 +0100

    added filter by company

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,28 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+  let companies = ['all', ...new Set(store.map((product) => product.company))];
+  const companiesDOM = getElement('.companies');
+  companiesDOM.innerHTML = companies
+    .map((company) => {
+      return `<button class="company-btn">${company}</button>`;
+    })
+    .join('');
+  companiesDOM.addEventListener('click', function (e) {
+    const element = e.target;
+    if (element.classList.contains('company-btn')) {
+      let filteredStore = [];
+      if (element.textContent === 'all') {
+        filteredStore = [...store];
+      } else {
+        filteredStore = store.filter(
+          (product) => product.company === e.target.textContent
+        );
+      }
+      display(filteredStore, getElement('.products-container'));
+    }
+  });
+};
 
 export default setupCompanies;
--- src/pages/products.js
+++ src/pages/products.js
@@ -12,4 +12,5 @@ const loading = getElement('.page-loading');
 
 display(store, getElement('.products-container'));
 setupSearch(store);
+setupCompanies(store);
 loading.style.display = 'none';
