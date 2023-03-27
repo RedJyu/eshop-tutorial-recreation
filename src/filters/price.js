218e0eb2f94ad29ecf9b833171f9951ae4fadbc2
RedJyu <piotr.stezycki@protonmai.com>
2023-03-27 12:49:21 +0200

    updated price

--- src/filters/price.js
+++ src/filters/price.js
@@ -1,6 +1,28 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupPrice = () => {};
+const setupPrice = (store) => {
+  const priceInput = getElement('.price-filter');
+  const priceValue = getElement('.price-value');
+
+  let maxPrice = store.map((product) => product.price);
+  maxPrice = Math.max(...maxPrice);
+  maxPrice = Math.ceil(maxPrice / 100);
+  priceInput.value = maxPrice;
+  priceInput.max = maxPrice;
+  priceInput.min = 0;
+  priceValue.textContent = `Price:$${maxPrice}`;
+
+  priceInput.addEventListener('input', function () {
+    const value = parseInt(priceInput.value);
+    priceValue.textContent = `Price:$${value}`;
+    let filteredStore = store.filter((product) => product.price / 100 <= value);
+    display(filteredStore, getElement('.products-container'));
+    if (filteredStore.length < 1) {
+      const products = getElement('.products-container');
+      products.innerHTML = `<h3>Sorry nothing matches your price range</h3>`;
+    }
+  });
+};
 
 export default setupPrice;
