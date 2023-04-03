4dd966fd3f74ef0ee8c8adea3ed4b6d009179874
RedJyu <piotr.stezycki@protonmai.com>
2023-04-03 11:52:28 +0200

    added add to cart function

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -8,7 +8,28 @@ import { openCart } from './toggleCart.js';
 import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 
+const cartItemCountDom = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
+
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    //add item
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    //display in DOM
+    addToCartDOM(product);
+  } else {
+  }
+
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+init();
